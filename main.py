57124c39ce9de5e233be777d49551bbd64248e49
Devergond <163804221+Devergond@users.noreply.github.com>
2025-04-17 16:24:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import os
 import telebot
-from flask import Flask, request
+import time
 
 TOKEN = os.getenv("TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -20,16 +17,10 @@ def handle_message(message):
     else:
         bot.send_message(message.chat.id, "Это не похоже на ссылку Reels.")
 
-@app.route("/" + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@app.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL + "/" + TOKEN)
-    return "Webhook set!", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+print("Bot started...")
+while True:
+    try:
+        bot.polling(non_stop=True, timeout=30)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        time.sleep(5)
